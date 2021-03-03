f11f462a1d588f96f9e43a78ce73b7ac453aec4d
Scott C <78004109+curry-scott@users.noreply.github.com>
2021-03-03 11:14:43 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,101 +1,102 @@
 
-let text = document.querySelector(".text");
+ 
+let text = document.querySelector('.text');//larger text to read
 let click = document.querySelector('.click');
-let subtext = document.querySelector('.subtext');
-let btn = document.querySelector('.home');
-
-let state = 1;
-
+let subtext = document.querySelector('.subtext');//smaller text
+let btn = document.querySelector('.btn');
+ 
+let state = 1;//start
+let finalAns;//the final string output
+ 
+//states or pages
 function state1() {
     text.innerHTML = 'I can read your mind';
-    click.innerHTML = 'Next';
+    click.innerHTML= '';
     subtext.innerHTML = '';
-    btn.innerHTML = '';
-
+    btn.innerHTML = 'GO';
+   
 }
-
+ 
 function state2() {
     text.innerHTML = 'Pick a number from 01-99';
     click.innerHTML = 'Next';
-    subtext.innerHTML = 'When you have your number, click next';
+    subtext.innerHTML = 'When you have your number click next';
     btn.innerHTML = '';
-    addHomeIcon();
-
+    addResetIcon();
 }
-
+ 
 function state3() {
     text.innerHTML = 'Add both digits together to get a new number';
     click.innerHTML = 'Next';
-    subtext.innerHTML = 'Ex: 14 is 1 + 4 = 5<br>Click next to proceed';
-    btn.innerHTML = '';
-    addHomeIcon();
+    subtext.innerHTML = 'Ex: 14 is 1+4=5<br>Click next to proceed';
+     btn.innerHTML = '';
+    addResetIcon();
 }
-
+ 
 function state4() {
     text.innerHTML = 'Subtract your new number from your old number';
-    click.innerHTML = 'Next';
-    subtext.innerHTML = 'Ex: 14 - 5 = 9 <br> Click next to proceed';
-    btn.innerHTML = '';
-    addHomeIcon();
+    click.innerHTML = 'NEXT';
+    subtext.innerHTML = 'Ex: 14-5=9<br>Click next to proceed';
+     btn.innerHTML = '';
+    addResetIcon();
 }
-
+ 
 function state5() {
-    text.innerHTML = symbols(10);
-    finalAns = text.textContent.slice(9); // Gets the final symbol answer
+    text.innerHTML = symbols(100);
+    finalAns = text.textContent.slice(49, 50);
+  
     click.innerHTML = 'Reveal';
-    subtext.innerHTML = 'Find your new number <br> Note the symbol beside the number';
-    btn.innerHTML = '';
-    addHomeIcon();
+    subtext.innerHTML = 'Find your new number<br>Note the symbol beside the nyumber';
+     btn.innerHTML = '';
+    addResetIcon();
 }
-
+ 
 function state6() {
-    text.innerHTML = '';
-    click.innerHTML = '';
-    subtext.innerHTML = `Your symbol is: <br> ${finalAns}`;
-    btn.innerHTML = '';
-    addHomeIcon();
+    text.innerHTML = finalAns;
+    subtext.innerHTML = `Your symbol is: ${finalAns}`;
+     btn.innerHTML = '';
+    addResetIcon();
 }
-
-let finalAns; // The final string output
-
+ 
 function clickNext() {
-    state++;
+    state ++;
     checkState(state);
 }
-
-function clickHome() {
-    if (btn.getAttribute('class') == 'go') {
-        state = 1;
-        btn.setAttribute('class', 'home');
+ 
+function clickReset() {
+    if (btn.getAttribute('class') === 'go') {
+        state = 2;
+      
         checkState(state);
     } else {
         state = 1;
-        btn.setAttribute('class', 'go');
+      
         checkState(state);
     }
 }
-
-function addHomeIcon() {
-    btn.innerHTML = ' <img src="img/undo-solid.svg" alt="home">';
-
+ 
+function addResetIcon() {
+    btn.innerHTML = 'Home';
+   
 }
-
+ 
 function symbols(x) {
     let output = '';
     let sym = [];
-    let symChar = ['!', '@', '#', '$', '%', '^', '&', '*', '=', '+'];
+    let symChar = ['=', '@', '*', '$', '%', '^', '&', '+', '#'];//array
     for (i = 0; i < 9; i++) {
-        let rand = (Math.floor(Math.random() * (9 - (9 - symChar.length)))); // rand num 0 - length of array
-        sym.push(symChar[rand]) // push into array
-        symChar.splice(rand, 0); // delete that symbol from array
+        let rand = (Math.floor(Math.random() * (9 - (9 - symChar.length))));//rand num 0 - length of array
+        sym.push(symChar[rand])//push into array
+        symChar.splice(rand, 1);//delete that symbol from array
     }
     for (i = 0; i < x; i++) {
         let num = i;
-        output += `${num} - ${sym[(i) % 9]}<br>`; // subtract 1 because arrays count from 0
+        output += `${num} - ${sym[(i) % 9]}<br>`;//subtract 1 because arrays count from 0
     }
     return output;
 }
-
+ 
+//checking states
 function checkState(stateNum) {
     if (stateNum == 1) {
         state1();
@@ -116,6 +117,8 @@ function checkState(stateNum) {
         state6();
     }
 }
-
+ 
 click.addEventListener('click', clickNext);
-btn.addEventListener('click', clickHome);
+btn.addEventListener('click', clickReset);
+ 
+ 
